d945ffc49894c05bcbea7668db36de5c81365160
на работе <andrey.tretiachenko@gmail.com>
2022-07-14 19:11:30 +0300

    надача ада

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from ftplib import FTP
 
 def main():
-    pass
+    structure = []
+    ftp = FTP("91.122.209.148", "admin", "Admin29")
+    ftp.cwd("Counter")
+    list_folder = ftp.nlst()
+    for l in list_folder:
+        ftp.cwd(l)
+        city = ftp.nlst()
+        structure.append([{'region': l, 'city': city}])
+        ftp.cwd("..")
+    print(structure[::1])
+
 
 if __name__ == '__main__':
     main()
