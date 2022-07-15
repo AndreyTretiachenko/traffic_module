442f5f13b4362f8d250b16b6188989857110ab45
andreytretiachenko <andrey.tretiachenko@gmail.com>
2022-07-15 23:42:19 +0300

    структуру поменял

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from ftplib import FTP
 
 def main():
-    structure = []
+    structure = [{
+        'region': ['region'],
+        'city': [{'region': 'region1', 'city': ['city1', 'city2', 'city3']},
+                 {'region': 'region2', 'city': ['city2', 'city3', 'city4']},
+                 {'region': 'region3', 'city': ['city5', 'city6', 'city7']}],
+        'company': [{'city': 'city1', 'company': ['company1', 'company2', 'company3']},
+                    {'city': 'city2', 'company': ['company4', 'company5', 'company6']}],
+        'shops': [{'city': 'city1', 'shops': ['shop1', 'shop2', 'shop3']},
+                  {'city': 'city2', 'shops': ['shop4', 'shop5', 'shop6']}],
+        'device': [{'shop': 'shop1', 'devices': ['device1']},
+                   {'shop': 'shop2', 'devices': ['device2', 'device2']}]
+    }]
     ftp = FTP("91.122.209.148", "admin", "Admin29")
     ftp.cwd("Counter")
     list_folder = ftp.nlst()
