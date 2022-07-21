dd89ff2b7c9044bc0113c5c7ada94a87c2bfac71
на работе <andrey.tretiachenko@gmail.com>
2022-07-21 18:56:53 +0300

    сервер работает, добавили модуль для отправки в базу данных

--- send_in_database.py
+++ send_in_database.py
@@ -1,6 +1,6 @@
 import requests as rq
 
-body = {"serial_number": "70-b3-d5-1a-05-48", "dateTime": "2022-07-21 15:43",
+body_1 = {"serial_number": "70-b3-d5-1a-05-48", "dateTime": "2022-07-21 15:43",
         "passReport": [{"id": 1,
                         "passList": [
                             {"passType": 2, "time": "15:43:33"},
@@ -31,13 +31,22 @@ def parse_body(body):
 def send_request(body_request):
     data_body = parse_body(body_request)
     if len(data_body) != 0:
+        for data in data_body:
+            request = rq.post("192.168.0.37:8123",
+                              params={
+                                  "database": "test",
+                                  "query": f"INSERT INTO traffic VALUES ("
+                                           f"{data['serial_number']}, {data['dateTime']}, {data['pass_type']}, {data['pass_time']}"
+                              }
+                              )
+            print(request.text)
 
     return
 
 
 
 def main():
-    parse_body(body)
+    send_request(body_1)
 
 
 
